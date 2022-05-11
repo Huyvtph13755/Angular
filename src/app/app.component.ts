0a4390cbe8274cf65001c9246df6f60e38843aca
Huy <huyvtph13755@fpt.edu.vn>
2022-05-11 07:10:55 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'My-app';
+  name = 'Huy';
+  class = 'WE16301';
+  students = [
+    {name: 'Huy', msv: 'ph13755'},
+    {name: 'Huy1', msv: 'ph137551'},
+    {name: 'Huy2', msv: 'ph137552'},
+    {name: 'Huy3', msv: 'ph137553'},
+    {name: 'Huy4', msv: 'ph137554'},
+    {name: 'Huy5', msv: 'ph137555'}
+  ]
 }
